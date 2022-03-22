e270266ef0e3a0a2a902a805eb3ab071008f57d7
Alexander Nitsch <alexander.nitsch@scs.ch>
2022-03-22 19:13:00 +0100

    Remove unused exception import

--- src/test/java/com/github/cleancoders/mockito/NumberPrinterUnitTests.java
+++ src/test/java/com/github/cleancoders/mockito/NumberPrinterUnitTests.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.concurrent.TimeoutException;
 
